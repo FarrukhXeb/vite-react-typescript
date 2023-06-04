fbd1b99b0ccd11b1cc2b74352a81a9593e964882
Farrukh Zeb <FarrukhXeb@gmail.com>
2023-06-05 02:16:09 +0500

    chore: initialize vite/react project

---
+++ src/App.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { WrappedApp, App } from './App';
+
+describe('App', () => {
+  it('Renders hello world', () => {
+    render(<WrappedApp />);
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello World');
+  });
+
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/route-not-exists']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Not found');
+  });
+});
